d5ba47717942d63ab42fbee07ee7357842ffa655
ZPrlmn <carlo.berks@gmail.com>
2024-05-15 00:23:35 +0800

    aaa

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const [timeoutId, setTimeoutId] = useState(null);
+  const [display, setDisplay] = useState('');
+  const [timer, setTimer] = useState(10);
+
+  const startTimer = () => {
+    const id = setTimeout(() => {
+      setDisplay("Timeout done");
+    }, 5000);
+    setTimeoutId(id);
+
+    const countDown = setInterval(() => {
+      setTimer(prevTimer => {
+        if (prevTimer === 1) {
+          clearInterval(countDown);
+        }
+        return prevTimer - 1;
+      });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearTimeout(timeoutId);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => clearTimeout(timeoutId); // Cleanup function to clear timeout when unmounting
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>{display}</Text>
+      <Text>{timer}</Text>
+      <Button title="Stop Timeout" onPress={stopTimer} />
+      <Button title='stop countdown'/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
